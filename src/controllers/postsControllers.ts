5f0743b27362dfbe53e1eb3c370e83d6375b129d
Arthur07Melo <aarthur00melo@gmail.com>
2023-04-13 19:43:23 -0300

    Added addPost route

--- src/controllers/postsControllers.ts
+++ src/controllers/postsControllers.ts
@@ -12,10 +12,36 @@ const getAllPosts = async (req: Request, res: Response) => {
     })
 }
 
+
+const addPost = async (req: Request, res: Response) => {
+    const reqBodyPattern = z.object({
+        title: z.string(),
+        description: z.string()
+    });
+
+    const { title, description } = reqBodyPattern.parse(req.body);
+
+    const newPost = await prisma.post.create({
+        data: {
+            title: title,
+            description: description,
+            user: {
+                connect: {
+                    id: req.user.id
+                }
+            }
+        }
+    });
+
+    res.status(201);
+    res.json(newPost);
+}
+
+
 const likePost = async (req: Request, res: Response) => {
     const reqParamsPattern = z.object({
         postID: z.number()
-    })
+    });
 
     const { postID } = reqParamsPattern.parse(req.params);
 
@@ -24,7 +50,7 @@ const likePost = async (req: Request, res: Response) => {
             userId: req.user.id,
             postId: postID
         }
-    })
+    });
 
     if(isLiked){
         await prisma.likePostUser.delete({
@@ -34,7 +60,7 @@ const likePost = async (req: Request, res: Response) => {
                     userId: req.user.id!
                 }
             }
-        })
+        });
 
         return res.status(200).json({ message: "likePostUser deleted" });
     }
@@ -57,4 +83,4 @@ const likePost = async (req: Request, res: Response) => {
     res.status(201).json({ message: "likePostUser created" });
 }
 
-export { getAllPosts, likePost }
+export { getAllPosts, addPost, likePost }
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { getAllPosts, likePost } from "../controllers/postsControllers"
+import { addPost, getAllPosts, likePost } from "../controllers/postsControllers"
 
 const router = express.Router();
 
 
-router.get("/:postID", likePost);
 router.get("/", getAllPosts);
-
+router.post("/", addPost);
+router.get("/:postID", likePost);
 
 
 export default router;
